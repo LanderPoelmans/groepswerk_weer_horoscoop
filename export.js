70f00fb3db2e945332940557d84341f8aa087f85
SanderGeunes <sandergeunes@gmail.com>
2022-11-22 09:10:52 +0100

    removed button function

--- export.js
+++ export.js
@@ -1,12 +1,6 @@
-document.getElementById("export").addEventListener('click', function () {
-    getInput();
-})
-
-function getInput() {
     let name_user = sessionStorage.getItem('name');
     let year = sessionStorage.getItem('year');
     let city = sessionStorage.getItem('city');
     console.log(year, city, name_user);
 
     document.getElementById('welcomefield').innerHTML = "<p>" + "Welcome, " + name_user + "</p>";
-}
